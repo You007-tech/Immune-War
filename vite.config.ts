21ec9a437962c5c99e1fec928ff0ff1b5f3ad8f3
You007-tech <3690680212@qq.com>
2025-12-28 14:54:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     // 如果是 GitHub Pages 部署，请将 'github' 模式下的 base 改为您的仓库名
     // 例如：mode === 'github' ? '/immune-war/' : '/'
     base: mode === 'github'
-      ? './' // 通用相对路径支持，适配多种托管平台
+      ? '/Immune-War/' // 通用相对路径支持，适配多种托管平台
       : '/',
     build: {
       outDir: 'dist',
